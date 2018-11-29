a73b40baab72d4ef63c14f4d42a43fba9d730a43
ilabstaff <ilabstaff@172.16.116.81>
2018-11-28 17:57:25 -0800

    secret message

---
+++ src/ifs_ints_and_loops/unbirthday.java
@@ -0,0 +1,35 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	String input=JOptionPane.showInputDialog("what is your birthday?" );
+	
+	if (input.equals("11/28")){
+		
+		
+		JOptionPane.showMessageDialog(null,"happy birthday!");	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	else {
+		JOptionPane.showMessageDialog(null,"HapPy unBirThday");	
+	}
+}
+}
